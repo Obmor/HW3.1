6dd6f6599c20755aa46bbd4d35ca64c3238e3b7f
Obmor <terekhin.alex@mail.ru>
2024-08-27 18:05:41 +0300

    FirstLook

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int a = 2;
+        int b = 3;
+        System.out.printf("Число 3 в квадрате равно %.0f \n", Math.pow(a, b));
+
+        double x1 = 10.635;
+        double y1 = 3.76;
+        System.out.printf("Значение %.3f в степени %.2f равно %.3f \n", x1, y1, Math.pow(x1, y1));
+        System.out.printf("pow(%.3f, %.3f) = %.3f \n", x1, y1, Math.pow(x1, y1));
+
+        int x2 = 2;
+        double y2 = 3.76;
+        System.out.printf("Значение 2 в степени %.2f равно %.3f \n", y2, Math.pow(x2, y2));
+        System.out.printf("pow(2, %.3f) = %.3f", y2, Math.pow(x2, y2));
     }
 }
